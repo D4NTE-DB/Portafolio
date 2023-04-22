6041d2fc256df4f4d7c40423462f48e1bb38eced
D4NTE-DB <dantebabilonia98@gmail.com>
2023-04-22 14:36:40 -0500

    update 21 widget

--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
+const INFO = [
+    {
+            header: {
+            name: "Daniel."
+        }, 
+        name: "Daniel Babilonia",
+        title: "Designer and Web Developer",
+        aboutme: "I'm a developer with experience in building websites for small and medium sized businesses. Whether you're trying to win work, list your services or even create a whole online store - I can help, I also worked for eBay as Bilingual Customer Service Representative for more than a year, so I can provide a really good insight when it comes from e-commerce platforms or UX-UI desings!",
+        technologies: [
+            {
+                title: "Javascript",
+                img: `<box-icon name='javascript' type='logo' size="large"></box-icon>`,
+            }
+        ]
+    },
+    
+       
+    
+    
+    ]
 const navBg = document.querySelector(".nav-bg");
 const navToggle = document.querySelector(".nav-toggle");
 const body = document.querySelector("body");
